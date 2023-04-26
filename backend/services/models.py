10dcea139ce312369623401c36663d575ab064e6
Sampsa Saloseutu <sampsa.saloseutu@hotmail.com>
2023-04-26 17:14:20 +0300

    Erased all 0001_inits and migrated the new db struct

--- backend/services/models.py
+++ backend/services/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from .models import Cabin
+from cabins.models import AreaCode
+
 
 class Service(models.Model):
-    area = models.ForeignKey(Cabin, on_delete=models.CASCADE)
+    area = models.ForeignKey(AreaCode, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=255)
     service_price = models.DecimalField(max_digits=5, decimal_places=2)
