d752e902f02ae47883c4a238466907ea519dd9ff
Jeb <commit@jeb4.dev>
2023-04-26 16:07:11 +0300

    formatted with black

--- backend/cabins/models.py
+++ backend/cabins/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
+
 class AreaCode(models.Model):
-    area = models.CharField(max_length = 100)
-    post_code = models.CharField(max_length = 10)
+    area = models.CharField(max_length=100)
+    post_code = models.CharField(max_length=10)
+
 
 class PostCode(models.Model):
-    p_code = models.ForeignKey(AreaCode, on_delete = models.CASCADE)
-    postal_district = models.CharField(max_length = 45)
-    
+    p_code = models.ForeignKey(AreaCode, on_delete=models.CASCADE)
+    postal_district = models.CharField(max_length=45)
+
 
 class Cabin(models.Model):
-    name = models.CharField(max_length = 20) # cabin name
-    description = models.TextField(max_length = 255) # freeform description of the cabin
-    price_per_night = models.DecimalField(max_digits = 8, decimal_places = 2) # renting price
-    area = models.ForeignKey(AreaCode, on_delete = models.CASCADE)
+    name = models.CharField(max_length=20)  # cabin name
+    description = models.TextField(max_length=255)  # freeform description of the cabin
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)  # renting price
+    area = models.ForeignKey(AreaCode, on_delete=models.CASCADE)
     num_of_beds = models.IntegerField()
 
     def __str__(self):
