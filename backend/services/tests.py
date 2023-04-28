2d13d498c0e27085ad7092dcfd3178accd8d599b
Sampsa Saloseutu <sampsa.saloseutu@hotmail.com>
2023-04-28 16:43:00 +0300

    Test case, not finished. Commented out problems

--- backend/services/tests.py
+++ backend/services/tests.py
@@ -2,15 +2,15 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Service
-from cabins.models import Cabin, AreaCode, PostCode
+from cabins.models import Cabin, Area, PostCode
 
 
 class ServiceTests(APITestCase):
     """Test the Service model"""
     def setUp(self):
         self.url=reverse('create_service')
-        self.area=AreaCode.objects.create(area="Helsinki", post_code = "00770")
-        #self.post=PostCode.objects.create(p_code=self.area, postal_district="Helsinki")
+        self.area=Area.objects.create(area="Helsinki")
+        #self.post=PostCode.objects.create(p_code='00070', postal_district="Helsinki")
         Service.objects.create(area=self.area,
                                 name="Sauna 66",
                                 description="Sauna 66 description",
@@ -23,12 +23,15 @@ class ServiceTests(APITestCase):
                                 service_price=45,
                                 vat_price=52)
 
-    def test_create_valid_service(self):
-        response = self.client.get(self.url)
+    """ def test_create_valid_service(self):
+        data = self.valid_service
+        print(self.valid_service)
+        response = self.client.post(self.url, data)
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
-        #self.assertEqual(response.data[0]['area'], self.area)
-        #self.assertEqual(response.data[1]['name'], 'Breakfast')
+        self.assertEqual(response.data[0]['area'], self.area)
+        self.assertEqual(response.data[1]['name'], 'Breakfast') """
 
 
     
