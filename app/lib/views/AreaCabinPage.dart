f13555fe198c2fd2f26e1885702526b9dc9a3bae
Jeb <commit@jeb4.dev>
2023-05-08 16:30:42 +0300

    cabin query from api

--- app/lib/views/AreaCabinPage.dart
+++ app/lib/views/AreaCabinPage.dart
@@ -1,5 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import '../data/Data.dart';
+import '../utils/auth.dart';
+
+class ResponseData {
+  final String message;
+  final List<Item>? data;
+
+  ResponseData(this.message, this.data);
+}
 
 class AreaItemsPage extends StatefulWidget {
   final Area area;
@@ -12,13 +24,48 @@ class AreaItemsPage extends StatefulWidget {
 
 class _AreaItemsPageState extends State<AreaItemsPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  List<Item> _selectedItems = [];
+  List<Item> _cabins = [];
   Item? _selectedItem;
   final TextEditingController _itemNameController = TextEditingController();
   final TextEditingController _itemPriceController = TextEditingController();
-  final TextEditingController _itemDescriptionController =
-      TextEditingController();
+  final TextEditingController _itemDescriptionController = TextEditingController();
   final TextEditingController _itemAddressController = TextEditingController();
+
+  Future<ResponseData> getAreaData() async {
+    try {
+      var token = await storage.read(key: 'jwt');
+      var response = await get(
+        Uri.parse('http://127.0.0.1:8000/api/area/cabins?area=${widget.area.name}'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+        },
+      );
+      var responseData = json.decode(response.body);
+
+      if (responseData['result'] == 'error') {
+        return ResponseData(responseData['message'], []);
+      }
+
+      _cabins.clear();
+
+      for (var c in responseData['data']) {
+        Item cabin = Item(
+          name: c['name'],
+          price: double.parse(c['price_per_night']),
+          description: c['description'],
+          address: c['address'],
+        );
+        _cabins.add(cabin);
+      }
+
+      return ResponseData(responseData['result'], _cabins);
+  }
+    catch (e) {
+      return ResponseData('error', []);
+    }
+  }
 
   void _onItemTap(Item item) {
     setState(() {
@@ -210,49 +257,122 @@ class _AreaItemsPageState extends State<AreaItemsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.area.name),
-      ),
-      body: ListView.builder(
-        itemCount: widget.area.items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final Item item = widget.area.items[index];
+        body: FutureBuilder<ResponseData>(
+          future: getAreaData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                ResponseData data = snapshot.data!;
+                List<Widget> cabinWidgets = [];
+                for (int i = 0; i < data.data!.length; i++) {
+                  cabinWidgets.add(
+                      ListTile(
+                        title: Text(data.data![i].name),
+                        subtitle: Text('Hinta: ${data.data![i].price}'),
+                        onTap: () {
+                          _onItemTap(data.data![i]);
+                        },
+                        trailing: Visibility(
+                          visible: true,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  _onItemEdit();
+                                },
+                                icon: const Icon(Icons.edit),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  _onItemDelete(data.data![i]);
+                                },
+                                icon: const Icon(Icons.delete),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                  );
+                }
+                if (data.data!.isEmpty) {
+                  cabinWidgets.add(
+                    Center(
+                      child: Column(children: [
+                        const SizedBox(height: 10),
+                        Text(
+                          "Mökkejä ei löytynyt!\n error: ${data.message}",
+                          style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ]),
+                    ),
+                  );
+                }
+                // display data here
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text(widget.area.name),
+                  ),
+                  body: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 10),
+                        const Text(
+                          "Mökit",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Expanded(
+                          child: ListView(
+                            children: cabinWidgets,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
 
-          return ListTile(
-            title: Text(item.name),
-            subtitle: Text('Hinta: ${item.price}'),
-            onTap: () {
-              _onItemTap(item);
-            },
-            trailing: Visibility(
-              visible: _selectedItem == item,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
+
+                  floatingActionButton: FloatingActionButton(
+                    child: const Icon(Icons.add),
                     onPressed: () {
-                      _onItemEdit();
+                      _showAddItemDialog();
                     },
-                    icon: const Icon(Icons.edit),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      _onItemDelete(item);
-                    },
-                    icon: const Icon(Icons.delete),
+                );
+              } else if (snapshot.hasError) {
+                // handle error here
+                String error = snapshot.error.toString();
+
+                return Scaffold(
+                  body: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Mökkejen haku epäonnistui!"),
+                        const SizedBox(height: 10),
+                        const Text(
+                            "Tarkista internet-yhteys ja yritä uudelleen!"),
+                        const SizedBox(height: 200),
+                        const Text("Virheilmoitus kehittäjälle:"),
+                        const SizedBox(height: 10),
+                        Text(error),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          _showAddItemDialog();
-        },
-      ),
+                );
+              }
+            }
+            // handle other connection states here
+            return CircularProgressIndicator();
+          },
+        ),
     );
   }
 }
