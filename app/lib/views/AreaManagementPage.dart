9c175f679f21ac7af5b49579ee2c5dac3fd7c859
Jeb <commit@jeb4.dev>
2023-05-08 16:29:50 +0300

    area addition and get
    Took 41 minutes

--- app/lib/views/AreaManagementPage.dart
+++ app/lib/views/AreaManagementPage.dart
@@ -1,8 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 import '../data/Data.dart';
-import 'AreaItemsPage.dart';
+import '../utils/auth.dart';
+import 'AreaCabinPage.dart';
 import 'AreaServicesPage.dart';
+
+class ResponseData {
+  final String message;
+  final List<Object>? data;
+
+  ResponseData(this.message, this.data);
+}
 
 class AreaManagementPage extends StatefulWidget {
   @override
@@ -14,6 +26,41 @@ class _AreaManagementPageState extends State<AreaManagementPage> {
   final List<Area> _areas = areas;
 
   final _areaNameController = TextEditingController();
+
+  Future<ResponseData> getAreaData() async {
+    try {
+      var token = await storage.read(key: 'jwt');
+      var response = await get(
+        Uri.parse('http://127.0.0.1:8000/api/area/'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+        },
+      );
+      var responseData = json.decode(response.body);
+
+      if (responseData['result'] == 'error') {
+        return ResponseData(responseData['message'], []);
+      }
+
+      _areas.clear();
+
+      for (var alue in responseData['data']) {
+        Area area = Area(
+          name: alue['area'],
+          items: [],
+          services: [],
+        );
+        _areas.add(area);
+      }
+
+      return ResponseData(responseData['result'], _areas);
+    }
+    catch (e) {
+      return ResponseData('error', []);
+    }
+  }
 
   void _renameArea(String newName) {
     setState(() {
@@ -111,7 +158,7 @@ class _AreaManagementPageState extends State<AreaManagementPage> {
             ),
             TextButton(
               child: const Text('Lisää'),
-              onPressed: () {
+              onPressed: () async {
                 if (_areaNameController.text.isEmpty) {
                   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                     content: Text('Syötä nimi!'),
@@ -119,19 +166,47 @@ class _AreaManagementPageState extends State<AreaManagementPage> {
                   ));
                   return;
                 }
-                setState(
-                      () {
-                    _areas.add(
-                      Area(
-                        name: _areaNameController.text,
-                        items: [],
-                        services: [],
-                      ),
-                    );
+                else {
+                    try {
+                      var token = await storage.read(key: 'jwt');
+                      var response = await post(
+                        Uri.parse('http://127.0.0.1:8000/api/area/create'),
+                        body: jsonEncode({
+                          'area': _areaNameController.text,
+                        }),
+                        headers: {
+                          "Content-Type": "application/json",
+                          "Accept": "application/json",
+                          HttpHeaders.authorizationHeader: 'Bearer $token',
+                        },
+                      );
+                      var data = json.decode(response.body);
+                      if (data['result'] == 'success') {
+                        Area area = Area(
+                          name: _areaNameController.text,
+                          items: [],
+                          services: [],
+                        );
+                        setState(() {
+                          _areas.add(area);
+                        });
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(
+                          content: Text('Alueen lisääminen epäonnistui! ' + data['message']),
+                          duration: const Duration(seconds: 6),
+                        ));
+                      }
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                            'Alueen lisääminen epäonnistui! ' + e.toString()),
+                        duration: const Duration(seconds: 6),
+                      ));
+                    }
 
                     _areaNameController.clear();
-                  },
-                );
+                  }
                 Navigator.of(context).pop();
               },
             ),
@@ -144,84 +219,137 @@ class _AreaManagementPageState extends State<AreaManagementPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 10),
-          const Text(
-            "Alueet",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _areas.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_areas[index].name),
-                  selected: _selectedIndex == index,
-                  onTap: () {
-                    setState(() {
-                      _selectedIndex = index;
-                    });
-                  },
-                  trailing: _selectedIndex == index
-                      ? Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  AreaItemsPage(area: _areas[index]),
-                            ),
-                          );
-                        },
-                        child: const Text('Mökit'),
+      body: FutureBuilder<ResponseData>(
+        future: getAreaData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              ResponseData data = snapshot.data!;
+              List<Widget> userWidgets = [];
+              if (data.data!.isEmpty) {
+                userWidgets.add(
+                  Center(
+                    child: Column(children: [
+                      const SizedBox(height: 10),
+                      Text(
+                        "Alueita ei löytynyt!\n error: ${data.message}",
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      const SizedBox(width: 8),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  AreaServicesPage(area: _areas[index]),
-                            ),
-                          );
-                        },
-                        child: const Text('Palvelut'),
-                      ),
-                      const SizedBox(width: 8),
-                      IconButton(
-                        onPressed: () {
-                          _showRenameDialog();
-                        },
-                        icon: const Icon(Icons.edit),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          _showDeleteDialog();
-                        },
-                        icon: const Icon(Icons.delete),
-                      ),
-                    ],
-                  )
-                      : null,
+                    ]),
+                  ),
                 );
-              },
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddAreaDialog();
+              }
+              // display data here
+              return Scaffold(
+                body: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Alueet",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _areas.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(_areas[index].name),
+                            selected: _selectedIndex == index,
+                            onTap: () {
+                              // setState(() {
+                              //   _selectedIndex = index;
+                              // });
+                            },
+                            trailing: true == true
+                                ? Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            AreaItemsPage(area: _areas[index]),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text('Mökit'),
+                                ),
+                                const SizedBox(width: 8),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            AreaServicesPage(area: _areas[index]),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text('Palvelut'),
+                                ),
+                                const SizedBox(width: 8),
+                                IconButton(
+                                  onPressed: () {
+                                    _showRenameDialog();
+                                  },
+                                  icon: const Icon(Icons.edit),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    _showDeleteDialog();
+                                  },
+                                  icon: const Icon(Icons.delete),
+                                ),
+                              ],
+                            )
+                                : null,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    _showAddAreaDialog();
+                  },
+                  tooltip: 'Lisää uusi alue',
+                  child: const Icon(Icons.add),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              // handle error here
+              String error = snapshot.error.toString();
+
+              return Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Alueiden haku epäonnistui!"),
+                      const SizedBox(height: 10),
+                      const Text(
+                          "Tarkista internet-yhteys ja yritä uudelleen!"),
+                      const SizedBox(height: 200),
+                      const Text("Virheilmoitus kehittäjälle:"),
+                      const SizedBox(height: 10),
+                      Text(error),
+                    ],
+                  ),
+                ),
+              );
+            }
+          }
+          // handle other connection states here
+          return CircularProgressIndicator();
         },
-        tooltip: 'Lisää uusi alue',
-        child: const Icon(Icons.add),
       ),
     );
   }
