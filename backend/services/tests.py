93f771a1a9790709c03fef64c43358d34af0cb99
Sampsa Saloseutu <sampsa.saloseutu@hotmail.com>
2023-05-09 15:17:45 +0300

    Service model tests completed.

--- backend/services/tests.py
+++ backend/services/tests.py
@@ -1,4 +1,4 @@
-from django.urls import reverse
+from decimal import Decimal
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Service
@@ -8,30 +8,34 @@ from cabins.models import Cabin, Area, PostCode
 class ServiceTests(APITestCase):
     """Test the Service model"""
     def setUp(self):
-        self.url=reverse('create_service')
         self.area=Area.objects.create(area="Helsinki")
         #self.post=PostCode.objects.create(p_code='00070', postal_district="Helsinki")
-        Service.objects.create(area=self.area,
-                                name="Sauna 66",
-                                description="Sauna 66 description",
-                                service_price=25,
-                                vat_price = 30)
-        
-        Service.objects.create(area=self.area, 
-                                name="Breakfast", 
-                                description="3 course breakfast",
-                                service_price=45,
-                                vat_price=52)
+        self.service = Service.objects.create(
+            area=self.area,
+            name="Sauna 66",
+            description="Sauna 66 description",
+            service_price=Decimal("25.00"),
+            vat_price=Decimal("30.00")
+        )
 
-    """ def test_create_valid_service(self):
-        data = self.valid_service
-        print(self.valid_service)
-        response = self.client.post(self.url, data)
-        print(response.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-        self.assertEqual(response.data[0]['area'], self.area)
-        self.assertEqual(response.data[1]['name'], 'Breakfast') """
+    def test_service_creation(self):
+        service_count = Service.objects.count()
+        self.assertEqual(service_count, 1)
+
+    def test_service_area(self):
+        self.assertEqual(self.service.area, self.area)
+
+    def test_service_name(self):
+        self.assertEqual(self.service.name, "Sauna 66")
+    
+    def test_service_description(self):
+        self.assertEqual(self.service.description, "Sauna 66 description")
+
+    def test_service_price(self):
+        self.assertEqual(self.service.service_price, Decimal("25.00"))
+
+    def test_service_vat_price(self):
+        self.assertEqual(self.service.vat_price, Decimal("30.00"))
 
 
     
