c86aefb4418f17e8e0235a8f394deb8d9e0e2f22
Jeb <commit@jeb4.dev>
2023-05-12 19:20:51 +0300

    filter raporrs

--- app/lib/views/RaportPage.dart
+++ app/lib/views/RaportPage.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:js_interop';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 import '../data/Data.dart';
 import '../data/User.dart';
@@ -24,6 +26,7 @@ class RaportPage extends StatefulWidget {
   List<Service> services = [];
   List<User> users = [];
   List<Cabin> cabins = [];
+  List<String> areas = [];
 }
 
 class _RaportPageState extends State<RaportPage> {
@@ -32,6 +35,10 @@ class _RaportPageState extends State<RaportPage> {
     super.initState();
     _fetchReservations();
   }
+
+  String? _selectedArea;
+  TextEditingController _startDateController = TextEditingController();
+  TextEditingController _endDateController = TextEditingController();
 
   Future<void> _fetchReservations() async {
     // if areas are already fetched, don't fetch again
@@ -158,14 +165,140 @@ class _RaportPageState extends State<RaportPage> {
       // Handle error
       print('Failed to fetch cabins: ${response.statusCode}');
     }
+
+    // fetch areas
+    response = await get(
+      Uri.parse('http://127.0.0.1:8000/api/area/'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var responseData = json.decode(response.body);
+      setState(() {
+        for (var area in responseData['data']) {
+          widget.areas.add(area['area'].toString());
+        }
+      });
+    } else {
+      // Handle error
+      print('Failed to fetch areas: ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Raportti'),
-        centerTitle: true,
+        toolbarHeight: 70,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              Expanded(
+                child: Tooltip(
+                  message: 'Valitse alue',
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: DropdownButton<String>(
+                      value: _selectedArea,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedArea = newValue;
+                        });
+                      },
+                      items: [
+                        DropdownMenuItem<String>(
+                          value: null,
+                          child: Text("Valitse alue"),
+                        ),
+                        ...widget.areas.map((String area) {
+                          return DropdownMenuItem<String>(
+                            value: area,
+                            child: Text(area),
+                          );
+                        }).toList(),
+                      ],
+                      underline: Container(),
+                      iconEnabledColor: Colors.black,
+                      style: TextStyle(color: Colors.black),
+                      isExpanded: true,
+                      hint: Text(_selectedArea ?? "Valitse alue"),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2015, 8),
+                      lastDate: DateTime(2101),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        _startDateController.text =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                      });
+                    }
+                  },
+                  child: AbsorbPointer(
+                    child: TextFormField(
+                      controller: _startDateController,
+                      decoration: const InputDecoration(
+                        labelText: 'Ensimmäinen päivä',
+                        hintText: 'Valitse raporttiajanjakson alku päivä',
+                        labelStyle: TextStyle(color: Colors.white),
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2015, 8),
+                      lastDate: DateTime(2101),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        _endDateController.text =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                      });
+                    }
+                  },
+                  child: AbsorbPointer(
+                    child: TextFormField(
+                      controller: _endDateController,
+                      decoration: const InputDecoration(
+                        labelText: 'Viimeinen päivä',
+                        hintText: 'Valitse raporttiajanjakson viimeinen päivä',
+                        labelStyle: TextStyle(color: Colors.white),
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: ListView(
         children: [
@@ -192,19 +325,30 @@ class _RaportPageState extends State<RaportPage> {
                 SizedBox(height: 20),
                 // calculate total income
                 Text(
-                    'Varauksen keskimääräinen tuotto: ${widget.reservations.fold(0, (previousValue, element) => (previousValue + element.cost! / widget.reservations.length).toInt())} €',
+                    'Varauksen keskimääräinen tuotto: ${widget.reservations.where((_reservation) => DateTime.parse(_reservation.startDate!).isAfter(DateTime.parse(_startDateController.text.isEmpty ? "1970-01-01" : _startDateController.text)) && DateTime.parse(_reservation.startDate!).isBefore(DateTime.parse(_endDateController.text.isEmpty ? "2100-01-01" : _endDateController.text))).fold(0, (previousValue, element) => (previousValue + element.cost! / widget.reservations.length).toInt())} €',
                     style: TextStyle(fontSize: 18)),
                 SizedBox(height: 20),
 // calculate income per cabin
                 SizedBox(height: 20),
                 Text(
-                    'Mökkien tuotto: ${widget.reservations.fold(0, (previousValue, element) => (previousValue + element.cost!).toInt())} €',
+                    'Mökkien tuotto: ${widget.reservations.where((_reservation) => DateTime.parse(_reservation.startDate!).isAfter(DateTime.parse(_startDateController.text.isEmpty ? "1970-01-01" : _startDateController.text)) && DateTime.parse(_reservation.startDate!).isBefore(DateTime.parse(_endDateController.text.isEmpty ? "2100-01-01" : _endDateController.text))).fold(0, (previousValue, element) => (previousValue + element.cost!).toInt())} €',
                     style: TextStyle(fontSize: 18)),
                 SizedBox(height: 20),
                 // calculate income per cabin
-                ...widget.cabins.map((cabin) {
+                ...widget.cabins.where((cabin) =>
+                (_selectedArea == null || _selectedArea == "Kaikki alueet" || cabin.area == _selectedArea)
+                ).map((cabin) {
                   var income = widget.reservations
-                      .where((reservation) => reservation.cabin == cabin.id)
+                      .where((reservation) =>
+                          reservation.cabin == cabin.id &&
+                          DateTime.parse(reservation.startDate!).isAfter(
+                              DateTime.parse(_startDateController.text.isEmpty
+                                  ? "1970-01-01"
+                                  : _startDateController.text)) &&
+                          DateTime.parse(reservation.startDate!).isBefore(
+                              DateTime.parse(_endDateController.text.isEmpty
+                                  ? "2100-01-01"
+                                  : _endDateController.text)))
                       .fold(
                           0,
                           (previousValue, element) =>
@@ -214,33 +358,38 @@ class _RaportPageState extends State<RaportPage> {
                 }),
                 SizedBox(height: 20),
                 Text(
-                    'Palveluiden tuotto yhteensä: ${widget.services.fold(0, (previousValue, element) => (previousValue + element.servicePrice).toInt())} €',
+                    'Palveluiden tuotto yhteensä: ${widget.services.where((service) => (_selectedArea == null || _selectedArea == "Kaikki alueet" || service.area == _selectedArea)).fold(0, (previousValue, element) => (previousValue + element.servicePrice).toInt())} €',
                     style: TextStyle(fontSize: 18)),
                 Text(
-                    'Keskimääräinen tuotto per palvelu: ${widget.services.length > 0 ? (widget.services.fold(0, (previousValue, element) => (previousValue + element.servicePrice / widget.services.length).toInt())) : 0} €',
+                    'Keskimääräinen tuotto per palvelu: ${widget.services.length > 0 ? (widget.services.where((service) => (_selectedArea == null || _selectedArea == "Kaikki alueet" || service.area == _selectedArea)).fold(0, (previousValue, element) => (previousValue + element.servicePrice / widget.services.length).toInt())) : 0} €',
                     style: TextStyle(fontSize: 21)),
                 SizedBox(height: 20),
                 // calculate income per service
-                ...widget.services.map((service) {
-                  var income = widget.reservations
-                      .where((reservation) =>
-                          reservation.services.contains(service.id))
-                      .fold(
-                          0,
-                          (previousValue, element) =>
-                              (previousValue + element.cost!).toInt());
+                ...widget.services.where((service) =>
+                (_selectedArea == null || _selectedArea == "Kaikki alueet" || service.area == _selectedArea)
+                ).map((service) {
+                  var income = (service.servicePrice).toInt();
                   return Text('${service.name}: $income €',
                       style: TextStyle(fontSize: 18));
                 }),
                 SizedBox(height: 20),
                 Text(
-                    'Keskimääräinen tuotto per asiakas: ${widget.users.length > 0 ? (widget.reservations.fold(0, (previousValue, element) => (previousValue + element.cost! / widget.users.length).toInt())) : 0} €',
+                    'Keskimääräinen tuotto per asiakas: ${widget.users.length > 0 ? (widget.reservations.where((_reservation) => DateTime.parse(_reservation.startDate!).isAfter(DateTime.parse(_startDateController.text.isEmpty ? "1970-01-01" : _startDateController.text)) && DateTime.parse(_reservation.startDate!).isBefore(DateTime.parse(_endDateController.text.isEmpty ? "2100-01-01" : _endDateController.text))).fold(0, (previousValue, element) => (previousValue + element.cost! / widget.users.length).toInt())) : 0} €',
                     style: TextStyle(fontSize: 21)),
                 SizedBox(height: 20),
                 // calculate income per customer
                 ...widget.users.map((user) {
                   var income = widget.reservations
-                      .where((reservation) => reservation.customer == user.id)
+                      .where((reservation) =>
+                          reservation.customer == user.id &&
+                          DateTime.parse(reservation.startDate!).isAfter(
+                              DateTime.parse(_startDateController.text.isEmpty
+                                  ? "1970-01-01"
+                                  : _startDateController.text)) &&
+                          DateTime.parse(reservation.startDate!).isBefore(
+                              DateTime.parse(_endDateController.text.isEmpty
+                                  ? "2100-01-01"
+                                  : _endDateController.text)))
                       .fold(
                           0,
                           (previousValue, element) =>
