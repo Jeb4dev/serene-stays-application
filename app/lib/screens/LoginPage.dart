4ed35bc6d37fbe2dab0ad982494fa2007a8da953
Jeb <commit@jeb4.dev>
2023-05-12 14:48:17 +0300

    fixed relative imports

--- app/lib/screens/LoginPage.dart
+++ app/lib/screens/LoginPage.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:serene_stays_app/screens/RegisterPage.dart';
+import 'RegisterPage.dart';
 
 import '../utils/auth.dart';
 import 'HomePage.dart';
--- app/lib/screens/RegisterPage.dart
+++ app/lib/screens/RegisterPage.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:serene_stays_app/screens/HomePage.dart';
+import 'HomePage.dart';
 
 import '../utils/auth.dart';
 import 'LoginPage.dart';
